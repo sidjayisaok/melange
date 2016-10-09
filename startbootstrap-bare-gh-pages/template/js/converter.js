26f495fecb5efd27fbc5a36239a1096d652ea77f
sidjayisaok <sidjay18@gmail.com>
2016-10-08 21:21:47 -0400

    minor revisions

--- startbootstrap-bare-gh-pages/template/js/converter.js
+++ startbootstrap-bare-gh-pages/template/js/converter.js
@@ -15,15 +15,15 @@ $().on('submit', function(){
 });
 
 
-var svg = d3.select("svg"),
+let svg = d3.select("svg"),
 margin = {top: 20, right: 20, bottom: 30, left: 40},
 width = +svg.attr("width") - margin.left - margin.right,
 height = +svg.attr("height") - margin.top - margin.bottom;
 
-var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+let x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
     y = d3.scaleLinear().rangeRound([height, 0]);
 
-var g = svg.append("g")
+let g = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.csv("/misc/data.csv", function(d) {
