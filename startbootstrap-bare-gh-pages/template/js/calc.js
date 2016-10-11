54fe70fafb0fa0befa15458e7190de27b6a586b8
sidjayisaok <sidjay18@gmail.com>
2016-10-11 17:58:32 -0400

    minor revisions

--- startbootstrap-bare-gh-pages/template/js/calc.js
+++ startbootstrap-bare-gh-pages/template/js/calc.js
@@ -1,10 +1,10 @@
 //vanilla script demo with es6
 const dummy = 0;
-document.querySelector('#solutionA').innerHTML = '<p>Grand total: $' + dummy + '</p>'; 
-document.querySelector('#solutionB').innerHTML = '<p>Amount split between ' + dummy + ' party members: $' + dummy + '</p>';
+document.querySelector('#solutionA').innerHTML = '<h6><p>Grand total: $' + dummy + '</p></h6>'; 
+document.querySelector('#solutionB').innerHTML = '<h6><p>Amount split between ' + dummy + ' party members: $' + dummy + '</p></h6>';
 document.querySelector('#solutionC').innerHTML = '<p><svg width="300" height="300" ><circle cx="150" cy="150" r="150" class="myTarget"/></svg></p>';
-document.querySelector('#solutionD').innerHTML = '<p>Total tip is : $' + dummy + '</p>';
-document.querySelector('#solutionE').innerHTML = '<p>Taxes paid is : $' + dummy + '</p>';
+document.querySelector('#solutionD').innerHTML = '<h6><p>Total tip is : $' + dummy + '</p></h6>';
+document.querySelector('#solutionE').innerHTML = '<h6><p>Taxes paid is : $' + dummy + '</p></h6>';
 
 //our d3 pie chart
 function pieGraph(total, percent, tax, answer, tip, taxTotal){
