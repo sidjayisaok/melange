1d181f9b5612380a3da15a09b5a3367e76da746c
sidjayisaok <sidjay18@gmail.com>
2016-10-11 14:33:30 -0400

    added d3 graphic for BMI calculator; unfinished

--- startbootstrap-bare-gh-pages/template/js/BMI.js
+++ startbootstrap-bare-gh-pages/template/js/BMI.js
@@ -5,5 +5,25 @@ class BMI{
     }
 }
 
+//d3 library working in conjunction with nv.d3.js
+nv.addGraph(function() {  
+  var chart = nv.models.bulletChart();
 
+  d3.select('#bulletChart svg')
+      .datum(bmiData())
+      .transition().duration(1000)
+      .call(chart);
+
+  return chart;
+});
+
+
+function bmiData() {
+  return {
+  	"title":"BMI",
+  	"ranges":[10,22,50],
+  	"measures":[22],
+  	"markers":[22]
+  };
+}
 
